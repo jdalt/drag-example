7ca4bcb37c29b307681aa510b4622c8fe8be2ab7
Jacob Dalton <jacob.dalton@sportngin.com>
2016-02-10 20:14:59 -0600

    create a table

--- drag-example.js
+++ drag-example.js
@@ -5,4 +5,13 @@ angular.module('drag-example', [])
 angular.module('drag-example')
 .controller('main-ctrl', function($scope) {
   $scope.test = 'world'
+
+  $scope.items = [
+    { name: "Division 1", price: "$5.00" },
+    { name: "Awesome Division", price: "$9.07" },
+    { name: "Dog Division", price: "$2.00" },
+    { name: "Cate Division", price: "$0.01" },
+    { name: "Mystery Land", price: "$8.00" }
+  ]
+
 })
